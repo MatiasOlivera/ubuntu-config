d36a1575167169773673ac0fa0e7bc23cb358597
Matias Olivera <matiasolivera@outlook.com>
2026-08-31 16:45:10 -0300

    feat: install oh my zsh

--- post-install.sh
+++ post-install.sh
@@ -5,3 +5,10 @@ sudo apt update
 
 # apply updates
 sudo apt upgrade
+
+DEVELOPMENT_DIR="$SCRIPT_DIR/development"
+
+source "$DEVELOPMENT_DIR/oh-my-zsh/oh-myzsh.sh"
+
+# Development
+install_oh_my_zsh
