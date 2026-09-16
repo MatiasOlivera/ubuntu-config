5a5f7df181a8a8ec59b028b2c53350ad2ef326fa
Matias Olivera <matiasolivera@outlook.com>
2026-09-16 02:38:35 -0300

    refactor(antigravity-cli): remove sourcing of .zshrc after PATH update

--- development/antigravity-cli/antigravity-cli-config.sh
+++ development/antigravity-cli/antigravity-cli-config.sh
@@ -2,5 +2,5 @@
 
 configure_antigravity_cli() {
     # to use the 'agy' CLI globally
-    echo 'export PATH="/home/matias/.local/bin:$PATH"' >>~/.zshrc && source ~/.zshrc
+    echo 'export PATH="/home/matias/.local/bin:$PATH"' >>~/.zshrc
 }
