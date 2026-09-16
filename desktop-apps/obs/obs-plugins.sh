7623de2da292f96817cff8a53472548df8f95b9b
Matias Olivera <matiasolivera@outlook.com>
2026-09-16 02:06:19 -0300

    refactor(obs): install plugins via loop

--- desktop-apps/obs/obs-plugins.sh
+++ desktop-apps/obs/obs-plugins.sh
@@ -1,24 +1,8 @@
 #!/usr/bin/env bash
 
 install_obs_plugins() {
-    # Advanced Scene Switcher
-    flatpak install flathub -y com.obsproject.Studio.Plugin.SceneSwitcher
-
-    # Advanced Masks
-    flatpak install flathub -y com.obsproject.Studio.Plugin.AdvancedMasks
-
-    # Move Transition
-    flatpak install flathub -y com.obsproject.Studio.Plugin.MoveTransition
-
-    # PipeWire Audio Capture
-    flatpak install flathub -y com.obsproject.Studio.Plugin.PipeWireAudioCapture
-
-    # Source Clone
-    flatpak install flathub -y com.obsproject.Studio.Plugin.SourceClone
-
-    # Stroke Glow Shadow
-    flatpak install flathub -y com.obsproject.Studio.Plugin.StrokeGlowShadow
-
-    # Source Copy
-    flatpak install flathub -y com.obsproject.Studio.Plugin.SourceCopy
+    local plugins=(SceneSwitcher AdvancedMasks MoveTransition PipeWireAudioCapture SourceClone StrokeGlowShadow SourceCopy)
+    for plugin in "${plugins[@]}"; do
+        flatpak install flathub -y "com.obsproject.Studio.Plugin.$plugin"
+    done
 }
